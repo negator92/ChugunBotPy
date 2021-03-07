8daed65597e1fbe04ac820dcb58b10ea37e48a05
Vincent Neparidze <negator92@gmail.com>
2021-03-07 17:22:49 +0300

    Updated paw, owm, wttr, cbr and code style

--- ChugunBotPy.py
+++ ChugunBotPy.py
@@ -3,9 +3,10 @@
 
 import json
 import platform
-
+import uuid
 import requests
 import telebot
+import os
 
 
 class Configuration(object):
@@ -22,11 +23,14 @@ botToken = config.botToken
 adminId = int(config.adminId)
 owmToken = config.owmToken
 
+lat = 55.716071
+lon = 37.790835
+
 bot = telebot.TeleBot(botToken)
 
 keyboardMarkup = telebot.types.ReplyKeyboardMarkup(True)
-keyboardMarkup.row('/about', '/help', '/now', '/paw')
-keyboardMarkup.row('/wttr', '/owm', '/cbr', '/ping')
+keyboardMarkup.row('/about', '/help', '/paw')
+keyboardMarkup.row('/wttr', '/owm', '/cbr')
 keyboardMarkup.row('/mem', '/cpu', '/temp')
 
 
@@ -34,80 +38,131 @@ def keyboard(chat_id):
     bot.send_message(
         chat_id,
         'Я могу:\nРассказать о себе: /about\nПомощь: /help\nДоступные команды: /keyboard',
-        reply_markup=keyboardMarkup)
+        reply_markup=keyboardMarkup, parse_mode='markdown')
 
 
-def paw():
-    response = requests.get(
-        f'https://www.pythonanywhere.com/api/v0/user/{config.pawUsername}/cpu/',
-        headers={'Authorization': 'Token {token}'.format(token=config.pawToken)})
-    msg = ''
-    j = response.json()
-    if response.status_code == 200:
-        msg += f'Dayli CPU limit seconds: {j["daily_cpu_limit_seconds"]}\n' \
-               f'Daily cpu total usage seconds: {j["daily_cpu_total_usage_seconds"]}\n' \
-               f'Next reset time: {j["next_reset_time"]}\n'
-    else:
-        msg += 'Got unexpected status code {}: {!r}'.format(response.status_code, response.json())
-    return msg
+def paw(chat_id):
+    try:
+        response = requests.get(
+            f'https://www.pythonanywhere.com/api/v0/user/{config.pawUsername}/cpu/',
+            headers={'Authorization': 'Token {token}'.format(token=config.pawToken)})
+        if response.status_code == 200:
+            j_paw = response.json()
+            msg = f'Dayli CPU limit seconds: *{j_paw["daily_cpu_limit_seconds"]}*\n' \
+                  f'Daily cpu total usage seconds: *{j_paw["daily_cpu_total_usage_seconds"]}*\n' \
+                  f'Next reset time: *{j_paw["next_reset_time"]}*\n'
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+        else:
+            msg = 'Got unexpected status code {}: {!r}'.format(response.status_code, response.json())
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+    except Exception as e:
+        mdg = f'"Exception (weather):", {e}'
+        bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
 
 
-def owm():
+def owm(chat_id):
 #    s_city = 'Moscow,RU'
 #    city_id = 524901
-    lat = 55.716071
-    lon = 37.790835
     try:
-        res = requests.get('http://api.openweathermap.org/data/2.5/weather',
+        response = requests.get('http://api.openweathermap.org/data/2.5/weather',
                            params={'lat': lat, 'lon': lon, 'units': 'metric', 'lang': 'ru', 'APPID': owmToken})
-        j_owm = res.json()
-        response = f'Осадки:{j_owm["weather"][0]["description"]}\n'
-        response += f'Температура:{j_owm["main"]["temp"]}\n'
-        response += f'Минимум:{j_owm["main"]["temp_min"]}\n'
-        response += f'Максимум:{j_owm["main"]["temp_max"]}\n'
-        return response
+        if response.status_code == 200:
+            j_owm = response.json()
+            msg = f'Осадки: *{j_owm["weather"][0]["description"]}*\n' \
+                  f'Температура: *{j_owm["main"]["temp"]}*\n' \
+                  f'Минимум: *{j_owm["main"]["temp_min"]}*\n' \
+                  f'Максимум: *{j_owm["main"]["temp_max"]}*\n'
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+        else:
+            msg = 'Got unexpected status code {}: {!r}'.format(response.status_code, response.json())
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
     except Exception as e:
-        return f'"Exception (weather):", {e}'
+        msg = f'"Exception (weather):", {e}'
+        bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message,
                  f'Я бот.\n' +
-                 f'Приятно познакомиться, {message.from_user.first_name}, ' +
-                 f'{message.from_user.last_name}, ' +
-                 f'{message.from_user.id}, ' +
-                 f'{message.from_user.username}\n' +
-                 f'Пульт управления: /keyboard')
+                 f'Приятно познакомиться, \n'
+                 f'First name: *{message.from_user.first_name}*, \n' +
+                 f'Last name: *{message.from_user.last_name}*, \n' +
+                 f'User id: *{message.from_user.id}*, \n' +
+                 f'Username: *{message.from_user.username}*\n' +
+                 f'Пульт управления: /keyboard',
+                 reply_markup=keyboardMarkup,
+                 parse_mode='markdown')
 
 
 def about(chat_id):
-    bot.send_message(chat_id,
-                     f'{platform.machine.__name__}: {platform.machine()}\n' +
-                     f'{platform.node.__name__}: {platform.node()}\n' +
-                     f'{platform.platform.__name__}: {platform.platform()}\n' +
-                     f'{platform.processor.__name__}: {platform.processor()}\n' +
-                     f'{platform.python_build.__name__}: {platform.python_build()}\n' +
-                     f'{platform.python_branch.__name__}: {platform.python_branch()}\n' +
-                     f'{platform.python_compiler.__name__}: {platform.python_compiler()}\n' +
-                     f'{platform.python_implementation.__name__}: {platform.python_implementation()}\n' +
-                     f'{platform.python_revision.__name__}: {platform.python_revision()}' +
-                     f'{platform.python_version.__name__}: {platform.python_version()}\n' +
-                     f'{platform.python_version_tuple.__name__}: {platform.python_version_tuple()}\n' +
-                     f'{platform.release.__name__}: {platform.release()}\n' +
-                     f'{platform.system.__name__}: {platform.system()}\n' +
-                     f'{platform.uname.__name__}: {platform.uname()}\n' +
-                     f'{platform.version.__name__}: {platform.version()}\n' +
-                     f'{platform.win32_edition.__name__}: {platform.win32_edition()}\n' +
-                     f'{platform.win32_is_iot.__name__}: {platform.win32_is_iot()}\n' +
-                     f'{platform.win32_ver.__name__}: {platform.win32_ver()}\n' +
-                     f'{platform._mac_ver_xml.__name__}: {platform._mac_ver_xml()}',
-                     reply_markup=keyboardMarkup)
+    msg = f'{platform.machine.__name__}: *{platform.machine()}*\n' \
+          f'{platform.node.__name__}: *{platform.node()}*\n' \
+          f'{platform.platform.__name__}: *{platform.platform()}*\n' \
+          f'{platform.processor.__name__}: *{platform.processor()}*\n' \
+          f'{platform.python_build.__name__}: *{platform.python_build()}*\n' \
+          f'{platform.python_branch.__name__}: *{platform.python_branch()}*\n' \
+          f'{platform.python_compiler.__name__}: *{platform.python_compiler()}*\n' \
+          f'{platform.python_implementation.__name__}: *{platform.python_implementation()}*\n' \
+          f'{platform.python_revision.__name__}: *{platform.python_revision()}*\n' \
+          f'{platform.python_version.__name__}: *{platform.python_version()}*\n' \
+          f'{platform.python_version_tuple.__name__}: *{platform.python_version_tuple()}*\n' \
+          f'{platform.release.__name__}: *{platform.release()}*\n' \
+          f'{platform.system.__name__}: *{platform.system()}*\n' \
+          f'{platform.uname.__name__}: *{platform.uname()}*\n' \
+          f'{platform.version.__name__}: *{platform.version()}*\n' \
+          f'{platform.win32_edition.__name__}: *{platform.win32_edition()}*\n' \
+          f'{platform.win32_is_iot.__name__}: *{platform.win32_is_iot()}*\n' \
+          f'{platform.win32_ver.__name__}: *{platform.win32_ver()}*\n' \
+          f'{platform._mac_ver_xml.__name__}: *{platform._mac_ver_xml()}*'
+    bot.send_message(
+        chat_id,
+        msg,
+        reply_markup=keyboardMarkup, parse_mode='markdown')
+
+
+def bot_help(chat_id):
+    msg = 'Use /keyboard to see all availiable commands.'
+    bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+
+
+def wttr_by_location(chat_id, lattitude=lat, longtitude=lon, lang='ru'):
+    try:
+        url = f'http://wttr.in/{lattitude}%2C{longtitude}_Mmtp_lang={lang}.png'
+        response = requests.get(url)
+        if response.status_code == 200:
+            guid = uuid.uuid4()
+            with open(f'./{guid}.png', 'wb') as f:
+                f.write(response.content)
+            bot.send_photo(chat_id, photo=open(f'./{guid}.png', 'rb'), reply_markup=keyboardMarkup, parse_mode='markdown')
+            os.remove(f'./{guid}.png')
+        else:
+            msg = 'Got unexpected status code {}: {!r}'.format(response.status_code, response.json())
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+    except Exception as e:
+        bot.send_message(chat_id, f'"Exception (weather):", {e}', reply_markup=keyboardMarkup, parse_mode='markdown')
+
+
+def cbr(chat_id, value='USD'):
+    try:
+        url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+        response = requests.get(url)
+        if response.status_code == 200:
+            j_cbr = response.json()
+            msg = f'{j_cbr["Timestamp"]} *{j_cbr["Valute"][value]["Name"]}*:\n' \
+                  f'сегодня *{j_cbr["Valute"][value]["Value"]}* рублей,\n' \
+                  f'но было *{j_cbr["Valute"][value]["Previous"]}* рублей'
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+        else:
+            msg = 'Got unexpected status code {}: {!r}'.format(response.status_code, response.json())
+            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup, parse_mode='markdown')
+    except Exception as e:
+        bot.send_message(chat_id, f'"Exception (weather):", {e}', reply_markup=keyboardMarkup, parse_mode='markdown')
 
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
-    text = message.text.upper()
+    text = message.text.split()[0].upper()
     chat_id = message.chat.id
     if chat_id == adminId:
         if text == '/KEYBOARD':
@@ -115,14 +170,24 @@ def text_handler(message):
         elif text == '/ABOUT':
             about(chat_id)
         elif text == '/PAW':
-            msg = paw()
-            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup)
+            paw(chat_id)
         elif text == '/OWM':
-            msg = owm()
-            bot.send_message(chat_id, msg, reply_markup=keyboardMarkup)
-#"/help', '/now', '/paw', '/wttr', '/owm', '/cbr', '/ping', '/mem', '/cpu', '/temp'"
+            owm(chat_id)
+        elif text == '/HELP':
+            bot_help(chat_id)
+        elif text == '/WTTR':
+            wttr_by_location(chat_id)
+        elif text == '/CBR':
+            cbr(chat_id)
+#'/mem', '/cpu', '/temp'"
         else:
             bot.send_message(chat_id, 'Do not understand.')
+
+
+wttr_by_location(adminId)
+owm(adminId)
+cbr(adminId)
+paw(adminId)
 
 
 print('Bot listening...')
